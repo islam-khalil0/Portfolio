a31c99c50bb943b9586df42912a67948d489abbe
islam <eslamkhalil851@gmail.com>
2023-05-26 03:50:33 +0300

    first commit

---
+++ src/components/Hero/Home.js
@@ -0,0 +1,57 @@
+import React from "react"
+import "./Home.css"
+import hero from "../Pic/mee.png"
+import skill1 from "../Pic/skill1.png"
+import skill2 from "../Pic/skill2.png"
+import skill3 from "../Pic/skill3.png"
+import { Typewriter } from "react-simple-typewriter"
+
+const Home = () => {
+  return (
+    <>
+      <section className='hero' id='home'>
+        <div className='container f_flex top'>
+          <div className='left top'>
+            <h3>WELCOME TO MY WORLD</h3>
+            <h1>
+              Hi, I’m <span>Islam Khalil</span>
+            </h1>
+            <h2>
+              a
+              <span>
+                <Typewriter words={[ " Software Developer." , " Professional Coder." , " FrontEnd Developer."]} loop cursor cursorStyle='|' typeSpeed={70} deleteSpeed={50} delaySpeed={1000} />
+              </span>
+            </h2>
+
+            <p>I use animation as a third dimension by which to simplify experiences and kuiding thro each and every interaction. I’m not adding motion just to spruce things up, but doing it in ways that.</p>
+
+            <div className='hero_btn d_flex'>
+              <div className='col_1'>
+                <h4>FIND WITH ME</h4>
+                <div className='button'>
+                  <button className='btn_shadow'>
+                    <i class='fab fa-facebook-f'></i>
+                  </button>
+                  <button className='btn_shadow'>
+                    <i class='fab fa-instagram'></i>
+                  </button>
+                  <button className='btn_shadow'>
+                    <i class='fab fa-linkedin-in'></i>
+                  </button>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+          <div className='right'>
+            <div className='right_img'>
+              <img src={hero} alt='' style={{width: '18rem' ,}}/>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Home
