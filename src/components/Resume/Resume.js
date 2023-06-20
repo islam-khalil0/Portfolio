378d9eede53f0b1b3d7d9919c5f3479e6f7b5dd7
islam <eslamkhalil851@gmail.com>
2023-06-20 19:03:14 +0300

    update resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,44 +1,58 @@
-import React from "react"
-import "./Resume.css"
-import ResumeApi from "./ResumeApi"
-import Card from "./Card"
+import React from "react";
+import "./Resume.css";
+import ResumeApi from "./ResumeApi";
+import Card from "./Card";
 
 const Resume = () => {
   return (
     <>
-      <section className='Resume' id='resume'>
-        <div className='container top'>
-          <div className='heading text-center'>
-            <h4 style={{fontSize:'.8rem'}}>7+ YEARS OF EXPERIENCE</h4>
-            <h1 style={{fontSize:'2.5rem'}}>My Resume</h1>
+      <section className="Resume" id="resume">
+        <div className="container top">
+          <div className="heading text-center">
+            <h1 style={{ fontSize: "2.5rem" }}>My Resume</h1>
           </div>
 
-          <div className='content-section mtop d_flex'>
-            <div className='left'>
-              <div className='heading'>
-                <h4 style={{fontSize:'.8rem'}}>2007-2010</h4>
-                <h1 style={{fontSize:'2rem'}} >Education Quality</h1>
+          <div className="content-section mtop d_flex">
+            <div className="left">
+              <div className="heading">
+                <h4 style={{ fontSize: ".8rem" }}>2021-2025</h4>
+                <h1 style={{ fontSize: "2rem" }}>E D U C A T I O N</h1>
               </div>
 
-              <div className='content'>
+              <div className="content">
                 {ResumeApi.map((val, id) => {
                   if (val.category === "education") {
-                    return <Card key={id} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />
+                    return (
+                      <Card
+                        key={id}
+                        title={val.title}
+                        year={val.year}
+                        rate={val.rate}
+                        desc={val.desc}
+                      />
+                    );
                   }
                 })}
-
               </div>
             </div>
-            <div className='left'>
-              <div className='heading'>
-                <h4 style={{fontSize:'.8rem'}}>2007-2010</h4>
-                <h1 style={{fontSize:'2rem'}}>Job Experience</h1>
+            <div className="left">
+              <div className="heading">
+                <h4 style={{ fontSize: ".8rem" }}>2021-Present</h4>
+                <h1 style={{ fontSize: "2rem" }}>E X P E R I E N C E</h1>
               </div>
 
-              <div className='content'>
+              <div className="content">
                 {ResumeApi.map((val, index) => {
                   if (val.category === "experience") {
-                    return <Card key={index} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />
+                    return (
+                      <Card
+                        key={index}
+                        title={val.title}
+                        year={val.year}
+                        rate={val.rate}
+                        desc={val.desc}
+                      />
+                    );
                   }
                 })}
               </div>
@@ -47,7 +61,7 @@ const Resume = () => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
